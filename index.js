e1abb3a74fdf8c7a57980402035894cdc1d7dc0d
zihadx <blankzihad@gmail.com>
2023-06-13 17:42:28 +0600

    instructClasses api made

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const instrucClassesCollection = client
+      .db("eduLightDb")
+      .collection("instrucClasses");
+    const selectedClassCollection = client
+      .db("eduLightDb")
+      .collection("selectedClass");
+
+    //instrucClasses apis
+    app.get("/instrucClasses", async (req, res) => {
+      const result = await instrucClassesCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +45,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
