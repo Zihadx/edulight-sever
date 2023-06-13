ecd9747fabddbaff698c45b2bb1d4ccaa25e1db3
zihadx <blankzihad@gmail.com>
2023-06-14 05:17:16 +0600

    admin apis create and work done

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
     const usersCollection = client.db("eduLightDb").collection("users");
 
     //users related apis
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
@@ -43,6 +49,18 @@ async function run() {
       }
 
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
